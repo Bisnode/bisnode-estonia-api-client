31f8f62edd905800ac217428289faea42cebb0db
Sergei Miami <miami@blackcrystal.net>
2014-04-21 16:51:56 +0300

    Added new functions Changed authorization Added output formats

--- lib/Bisnode_Client.class.php
+++ lib/Bisnode_Client.class.php
@@ -1,30 +1,29 @@
 <?php
 
 /**
-* Copyright 2014 Bisnode Estonia, Inc.
-*
-* Licensed under the Apache License, Version 2.0 (the "License"); you may
-* not use this file except in compliance with the License. You may obtain
-* a copy of the License at
-*
-* http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-* License for the specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright 2014 Bisnode Estonia, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+class Bisnode_Client {
 
-class Bisnode_Client
-{
-  
-  private $api_url   = 'http://localhost/api', //'http://www.bisnode.ee/intranet/api';
-          $api_mode  = 'json',
-          $client    = null,
-          $debug     = true
-          ;
-  
+  private $api_key = '',
+          $api_url = 'http://www.bisnode.ee/intranet/api',
+          $api_mode = 'xml',
+          $client = null,
+          $debug = false,
+          $raw_data = ''
+  ;
   private static $instance = null;
 
   /**
@@ -33,6 +32,7 @@ class Bisnode_Client
    */
   private function __construct()
   {
+    // do nothing
   }
 
   /**
@@ -41,123 +41,184 @@ class Bisnode_Client
    */
   public static function getInstance()
   {
-    return is_null(self::$instance) 
-      ? self::$instance = new self()
-      : self::$instance;
+    return is_null(self::$instance) ? self::$instance = new self() : self::$instance;
   }
 
-  /**
-   * Login with api_key
-   * @param type $api_key must be generated in www.bisnode.ee/intranet/api
-   * @return bool success
-   */
-  public function login($api_key)
+  public function __toString()
   {
-    $result = $this->_request('login', array('key'=>$api_key));
-    return isset($result->success) ? true : false;
-  }
-
-  /**
-   * Logout
-   * @return bool success
-   */
-  public function logout()
-  {
-    $result = $this->_request('logout');
-    return $result->success ? true : false;
-  }
-  
-  /**
-   * Check client permissions
-   * @return string
-   */
-  public function permission()
-  {
-    $result = array();
-    foreach ($this->_request('permission') as $item)
-      $result = $item.'';
-    return $result;
-  }
-  
-  /**
-   * Set mode of requests
-   * @param string $api_mode 'xml' or 'json' is now supported
-   * @return Bisnode_Client
-   */
-  public function setMode($api_mode)
-  {
-    $this->api_mode = $api_mode;
-    return $this;
+    return $this->asIs();
   }
   
   /**
    * Get company short report
    * @param type $reg_code
-   * @return type
+   * @return Bisnode_Client
    */
   public function getShortReport($reg_code)
   {
-    $result = $this->_request('shortreport', array('id'=>$reg_code));
-    return $result;
+    $result = $this->_request('shortreport', array('id' => $reg_code));
+    return $this;
+  }
+
+  /**
+   * Get company kmkr data from maksuamet
+   * @param type $reg_code
+   * @return Bisnode_Client
+   */
+  public function getKmkrData($reg_code)
+  {
+    $result = $this->_request('kmkrData', array('id' => $reg_code));
+    return $this;
+  }
+
+  /**
+   * Get tax arrears information from Maksuamet
+   * @param type $reg_code
+   * @return Bisnode_Client
+   */
+  public function getTaxArrears($reg_code)
+  {
+    $result = $this->_request('taxArrears', array('id' => $reg_code));
+    return $this;
+  }
+
+  /**
+   * Get list of documents from Ariregister
+   * @param type $reg_code
+   * @return Bisnode_Client
+   */
+  public function getArListDocs($reg_code)
+  {
+    $result = $this->_request('arListDocs', array('id' => $reg_code));
+    return $this;
+  }
+
+  /**
+   * Get list of annual reports from Ariregister
+   * @param type $reg_code
+   * @return Bisnode_Client
+   */
+  public function getArListReports($reg_code)
+  {
+    $result = $this->_request('arListReports', array('id' => $reg_code));
+    return $this;
+  }
+
+  /**
+   * Get raw data from last request
+   * @return string
+   */
+  public function asIs()
+  {
+    return $this->raw_data;
+  }
+
+  /**
+   * Get XML data from last request
+   * @return SimpleXMLElement
+   */
+  public function asXml()
+  {
+    return simplexml_load_string($this->asIs(), null, LIBXML_NOCDATA);
+  }
+
+  
+  /**
+   * Format result as JSON
+   * @return stdClass
+   */
+  public function asJson()
+  {
+    $json = json_encode($this->asXml());
+    return str_replace('{}', 'null', $json);
+  }
+  
+  
+  /**
+   * Get data last result as Object
+   * @return stdClass
+   */
+  public function asObject()
+  {
+    return json_decode($this->asJson());
+  }
+
+  /**
+   * Get data from last request as Array
+   * @return array
+   */
+  public function asArray()
+  {
+    return json_decode($this->asJson(), true);
+  }
+
+  /**
+   * Change api_key
+   * @param type $api_key must be generated in www.bisnode.ee/intranet/api
+   * @return Bisnode_Client
+   */
+  public function setApiKey($api_key)
+  {
+    $this->api_key = $api_key;
+    return $this;
+  }
+
+  /**
+   * Change api URL
+   * @param $url string
+   * @return Bisnode_Client
+   */
+  public function setApiUrl($url)
+  {
+    $this->api_url = $url;
+    return $this;
   }
 
   /**
    * Request API with post or without
    * @param string $url
    * @param array|string $post
-   * @return SimpleXMLElement or object
+   * @return nothing
    */
-  private function _request( $url, $post = null )
+  private function _request($url, $post = null)
   {
-    
-    $wrap = $this->api_mode == 'json' 
-            ? 'json_decode' 
-            : 'SimpleXMLElement';
-    
-    $url =  $this->api_url .'/'. $url . '.' . $this->api_mode;
-    
-    if (is_null($this->client)) $this->_curl_init ();
-    
-    $this->_log('POST '. $url .' '. print_r($post,true));
-    
+    if (is_null($this->client))
+      $this->_curl_init();
+
+    $url = $this->api_url . '/' . $this->api_key . '/' . $url . '.' . $this->api_mode;
+
+    $this->_log('POST ' . $url . ' ' . print_r($post, true));
+
     curl_setopt($this->client, CURLOPT_URL, $url);
     curl_setopt($this->client, CURLOPT_POSTFIELDS, $post);
-    $data = curl_exec($this->client);
-    
-    $this->_log($data."\n");
-    
-    if ($wrap)
-     $data = class_exists($wrap) ? new $wrap($data) : call_user_func($wrap,$data);
-    
-    return $data;
-    
+    $this->raw_data = curl_exec($this->client);
+
+    $this->_log($this->raw_data . "\n");
   }
-  
-  
+
+  /**
+   * Init cURL
+   * @return type
+   */
   private function _curl_init()
   {
-    // create cookie file in temp directory
-    $cookieJar = sys_get_temp_dir() . '/curl_cookies';
-    if (!is_writable($cookieJar)) 
-      file_put_contents($cookieJar, '') && chmod($cookieJar, 0777);
-
-    // init curl
+    if ($this->client)
+      return;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_COOKIEJAR, $cookieJar);
-    curl_setopt($ch, CURLOPT_COOKIEFILE, $cookieJar);
     $this->client = $ch;
   }
-  
+
   /**
    * Log message for debug
    * @param string $data
    */
   private function _log($message)
   {
-    if (!$this->debug) return;
-    echo date('Y-m-d H:i:s') . ' '. substr($message, 0, 255) ."\n";
+    if (!$this->debug)
+      return;
+    echo date('Y-m-d H:i:s') . ' ' . substr($message, 0, 255) . "\n";
   }
-  
+
 }
